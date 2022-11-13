660f757206b31a4846e930340b4934e735ba6eeb
Jon <jonathan.p.lamar@gmail.com>
2022-11-13 16:13:07 -0700

    Getting array from camera

--- src/online/utils/robot_driver.py
+++ src/online/utils/robot_driver.py
@@ -1,10 +1,16 @@
 from online.impl import RobotState
+import picamera
+import picamera.array
 
 
 class RobotDriver:
     def __init__(self) -> None:
-        # TODO: Initialize EasyGoPiGo instance here
-        pass
+        self.camera = picamera.PiCamera()
+        self.camera.resolution = (640, 480)
+
 
     def takeSensorReading(self) -> RobotState:
+        with picamera.array.PiRGBArray(self.camera) as output:
+            self.camera.capture(output, 'rgb')
+            rgbArray = output.array
         raise NotImplementedError

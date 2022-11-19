247522e59ad32e2e5ffca82309366b6478dd6446
Jon <jonathan.p.lamar@gmail.com>
2022-11-19 12:09:03 -0700

    bigfixes

--- rl_infra/edge/utils.py
+++ rl_infra/edge/utils.py
@@ -19,20 +19,3 @@ def compress_nparr(nparr):
 def uncompress_nparr(bytestring):
     """ """
     return np.load(io.BytesIO(zlib.decompress(bytestring)))
-
-
-# This would be nice to handle on the edge, but I don't know how to make flask
-# subproc out and keep track of the PID
-# def exploreRoutine():
-#     distanceSensor.start_continuous()
-#     goPiGo.forward()
-#     while True:
-#         dist = distanceSensor.read_range_continuous()
-#         print(dist)
-#         if dist < 75:
-#             goPiGo.stop()
-#             goPiGo.drive_cm(-20)
-#             direction = 2 * round(random.random()) - 1
-#             goPiGo.turn_degrees(direction * 20)
-#             if distanceSensor.read_range_continuous() >= 30:
-#                 goPiGo.forward()

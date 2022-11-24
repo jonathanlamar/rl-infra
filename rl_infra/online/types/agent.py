88fd7fa0d5e62e3ace907fc97ecfd31a4147dbca
Jon <jonathan.p.lamar@gmail.com>
2022-11-24 08:27:19 -0700

    Parameterizing StepOutcome properly

--- rl_infra/online/types/agent.py
+++ rl_infra/online/types/agent.py
@@ -2,7 +2,7 @@ from typing import Protocol, TypeVar
 
 from ..types.environment import Action, State, StepOutcome
 
-S = TypeVar("S", bound=State, covariant=False, contravariant=True)
+S = TypeVar("S", bound=State, covariant=False, contravariant=False)
 A = TypeVar("A", bound=Action, covariant=False, contravariant=False)
 
 
@@ -10,5 +10,5 @@ class Agent(Protocol[S, A]):
     def chooseAction(self, state: S) -> A:
         raise NotImplementedError
 
-    def updatePolicy(self, state: S, action: A, outcome: StepOutcome) -> None:
+    def updatePolicy(self, state: S, action: A, outcome: StepOutcome[S]) -> None:
         raise NotImplementedError

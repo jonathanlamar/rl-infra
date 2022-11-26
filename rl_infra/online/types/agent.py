31763bee2efa78166ec84c3bd8d6b8e1bb99bd7d
Jon <jonathan.p.lamar@gmail.com>
2022-11-25 17:59:34 -0700

    Tweaking agent type to match secret work on tetris

--- rl_infra/online/types/agent.py
+++ rl_infra/online/types/agent.py
@@ -1,14 +1,24 @@
+import random
 from typing import Protocol, TypeVar
 
-from ..types.environment import Action, State, Transition
+from ..types.environment import Action, State
 
-S = TypeVar("S", bound=State, covariant=False, contravariant=False)
-A = TypeVar("A", bound=Action, covariant=False, contravariant=False)
+S = TypeVar("S", bound=State, covariant=False, contravariant=True)
+A = TypeVar("A", bound=Action, covariant=True, contravariant=False)
 
 
 class Agent(Protocol[S, A]):
+    epsilon: float
+
     def chooseAction(self, state: S) -> A:
+        """Choose action in epsilon-greedy manner, according to policy"""
+        if random.random() < self.epsilon:
+            return self.chooseRandomAction()
+        else:
+            return self.choosePolicyAction(state)
+
+    def choosePolicyAction(self, state: S) -> A:
         raise NotImplementedError
 
-    def updatePolicy(self, transition: Transition[S, A]) -> None:
+    def chooseRandomAction(self) -> A:
         raise NotImplementedError

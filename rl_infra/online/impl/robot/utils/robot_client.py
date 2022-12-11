52b3d948d7237c7cb0832e73130a2d3f97749e8d
Jonathan Lamar <jonalarm@amazon.com>
2022-12-11 10:17:44 -0700

    Partially implementing client and updating service.  Committing to test

--- rl_infra/online/impl/robot/utils/robot_client.py
+++ rl_infra/online/impl/robot/utils/robot_client.py
@@ -1,3 +1,4 @@
+from dataclasses import dataclass
 import json
 from typing import Tuple
 
@@ -7,6 +8,14 @@ import requests
 
 from .....edge import config
 from .....edge.utils import uncompress_nparr
+
+
+@dataclass
+class RobotSensorReading:
+    image: ndarray
+    distance: int
+    motionDetected: bool
+    lightColor: Tuple[float, float, float, float]
 
 
 class RobotClient:
@@ -26,21 +35,65 @@ class RobotClient:
             print(response.content.decode("utf-8"))
 
     @staticmethod
-    def getSensorReading() -> Tuple[ndarray, int]:
+    def getSensorReading() -> RobotSensorReading:
+        img = RobotClient._getImage()
+        dist = RobotClient._getDistance()
+        motionDetected = RobotClient._getMotion()
+        lightColor = RobotClient._getLightColorReading()
+
+        return RobotSensorReading(
+            image=img,
+            distance=dist,
+            motionDetected=motionDetected,
+            lightColor=lightColor,
+        )
+
+    @staticmethod
+    def _getImage() -> ndarray:
         imgResponse = requests.get(
             url=RobotClient.url + config.IMG_PATH,
             headers={"Content-Type": "application/octet-stream"},
         )
         if imgResponse.status_code != 200:
             raise requests.HTTPError("Failed to get image")
-        img = uncompress_nparr(imgResponse.content)
 
+        return uncompress_nparr(imgResponse.content)
+
+    @staticmethod
+    def _getDistance() -> int:
         distResponse = requests.get(url=RobotClient.url + config.DIST_PATH)
         if distResponse.status_code != 200:
             raise requests.HTTPError("Failed to get distance reading")
-        dist = int(distResponse.content)
+        return int(distResponse.content)
 
-        return img, dist
+    @staticmethod
+    def _getMotion() -> bool:
+        motionResponse = requests.get(url=RobotClient.url + config.MOTION_PATH)
+        if motionResponse.status_code != 200:
+            raise requests.HTTPError("Failed to get motion reading")
+        return motionResponse.content == "True"
+
+    @staticmethod
+    def _getLightColorReading() -> Tuple[float, float, float, float]:
+        lightColorResponse = requests.get(url=RobotClient.url + config.LIGHT_COLOR_PATH)
+        if lightColorResponse.status_code != 200:
+            raise requests.HTTPError("Failed to get light and color reading")
+        return lightColorResponse.content
+
+    @staticmethod
+    def _rotateMast(heading: int):
+        if heading < 0 or heading > 180:
+            raise ValueError("Impossible heading")
+        data = {"heading": heading}
+        response = requests.post(
+            url=RobotClient.url + config.SERVO_PATH,
+            data=json.dumps(data),
+            headers={"Content-type": "application/json"},
+        )
+        if response.status_code != 200:
+            raise requests.HTTPError("Failed to get distance reading")
+        else:
+            print(response.content.decode("utf-8"))
 
     @staticmethod
     def saveArrayAsJpeg(img: ndarray, filePath: str) -> None:
@@ -48,36 +101,3 @@ class RobotClient:
             img = img.astype(uint8)
         im = Image.fromarray(img)
         im.save(filePath)
-
-    @staticmethod
-    def buzz(numberOfTones: int) -> None:
-        data = {"numberOfTones": numberOfTones}
-        response = requests.post(
-            url=RobotClient.url + config.BUZZ_PATH,
-            data=json.dumps(data),
-            headers={"Content-type": "application/json"},
-        )
-        if response.status_code != 200:
-            raise requests.HTTPError("Failed to activate buzzer")
-
-    @staticmethod
-    def blinkLed(numberOfBlinks: int) -> None:
-        data = {"numberOfBlinks": numberOfBlinks}
-        response = requests.post(
-            url=RobotClient.url + config.LIGHT_PATH,
-            data=json.dumps(data),
-            headers={"Content-type": "application/json"},
-        )
-        if response.status_code != 200:
-            raise requests.HTTPError("Failed to light LED")
-
-    @staticmethod
-    def switchLed() -> None:
-        data = {"switchLed": True}
-        response = requests.post(
-            url=RobotClient.url + config.LIGHT_PATH,
-            data=json.dumps(data),
-            headers={"Content-type": "application/json"},
-        )
-        if response.status_code != 200:
-            raise requests.HTTPError("Failed to switch LED")

c89a18d24c216e9b5d6402c753275b4006522997
Jonathan Lamar <jonalarm@amazon.com>
2022-12-11 08:34:25 -0700

    Adding methods for new sensors and making robot service OOP

--- rl_infra/edge/config.py
+++ rl_infra/edge/config.py
@@ -1,9 +1,10 @@
-BUZZ_PATH = "/buzz"
 DIST_PATH = "/distance"
 EXPLORE_PATH = "/explore"
 IMG_PATH = "/camera"
-LIGHT_PATH = "/light"
+LIGHT_COLOR_PATH = "/light_color"
 LOCAL_IP = "0.0.0.0"
+MOTION_PATH = "/motion"
 MOVE_PATH = "/action"
 SERVER_HOST = "dex.local"
 SERVER_PORT = 8080
+SERVO_PATH = "/servo"
--- rl_infra/edge/robot_service.py
+++ rl_infra/edge/robot_service.py
@@ -12,94 +12,114 @@ from .utils import compress_nparr
 camera = picamera.PiCamera()
 camera.resolution = (640, 480)
 goPiGo = EasyGoPiGo3()
-distanceSensor = goPiGo.init_distance_sensor()
-buzzer = goPiGo.init_buzzer(port="AD1")
-led = goPiGo.init_led(port="AD2")
-app = Flask(__name__)
+distanceSensor = goPiGo.init_distance_sensor(port="AD2")
+motionSensor = goPiGo.init_motion_sensor(port="AD1")
+lightColorSensor = goPiGo.init_light_color_sensor()
+servo = goPiGo.init_servo()
 
 
-@app.route(config.IMG_PATH, methods=["GET"])
-def sendCompressedImage():
-    with picamera.array.PiRGBArray(camera) as output:
-        camera.capture(output, "rgb")
-        rgbArray = output.array
-    resp, _, _ = compress_nparr(rgbArray)
+class RobotService:
+    def __init__(self):
+        self.app = Flask(__name__)
+        self.app.add_url_rule(
+            rule=config.IMG_PATH,
+            view_func=RobotService.sendCompressedImage,
+            methods=["GET"],
+        )
+        self.app.add_url_rule(
+            rule=config.DIST_PATH,
+            view_func=RobotService.sendCompressedImage,
+            methods=["GET"],
+        )
+        self.app.add_url_rule(
+            rule=config.MOTION_PATH,
+            view_func=RobotService.sendCompressedImage,
+            methods=["GET"],
+        )
+        self.app.add_url_rule(
+            rule=config.LIGHT_COLOR_PATH,
+            view_func=RobotService.sendCompressedImage,
+            methods=["GET"],
+        )
+        self.app.add_url_rule(
+            rule=config.SERVO_PATH,
+            view_func=RobotService.sendCompressedImage,
+            methods=["POST"],
+        )
+        self.app.add_url_rule(
+            rule=config.MOVE_PATH,
+            view_func=RobotService.sendCompressedImage,
+            methods=["POST"],
+        )
 
-    return Response(response=resp, status=200, mimetype="application/octet_stream")
+    def run(self):
+        self.app.run(host=config.SERVER_HOST, port=config.SERVER_PORT)
 
+    @staticmethod
+    def sendCompressedImage():
+        with picamera.array.PiRGBArray(camera) as output:
+            camera.capture(output, "rgb")
+            rgbArray = output.array
+        resp, _, _ = compress_nparr(rgbArray)
 
-@app.route(config.DIST_PATH, methods=["GET"])
-def sendDistanceReading():
-    resp = str(distanceSensor.read_mm())
+        return Response(response=resp, status=200, mimetype="application/octet_stream")
 
-    return Response(response=resp, status=200)
+    @staticmethod
+    def sendDistanceReading():
+        resp = str(distanceSensor.read_mm())
 
+        return Response(response=resp, status=200)
 
-@app.route(config.MOVE_PATH, methods=["POST"])
-def move():
-    if request.json is None:
-        return Response(response="Bad request format", status=400)
+    @staticmethod
+    def sendMotionReading():
+        resp = str(motionSensor.motion_detected())
 
-    action = request.json["action"]
-    arg = int(request.json["arg"])
+        return Response(response=resp, status=200)
 
-    if action == "move":
-        goPiGo.drive_cm(arg)
-        resp = "Moving {} cm".format(arg)
-        status = 200
-    elif action == "turn":
-        goPiGo.turn_degrees(arg)
-        resp = "Turning {} deg".format(arg)
-        status = 200
-    else:
-        resp = "Bad action request"
-        status = 400
+    @staticmethod
+    def getLightColorReading():
+        resp = lightColorSensor.safe_raw_colors()
 
-    return Response(response=resp, status=status)
+        return Response(response=resp, status=200)
 
+    @staticmethod
+    def rotateMast():
+        if request.json is None:
+            return Response(response="Bad request format", status=400)
 
-@app.route(config.BUZZ_PATH, methods=["POST"])
-def buzz():
-    if request.json is None:
-        return Response(response="Bad request format", status=400)
+        degrees = request.json["degrees"]
+        if degrees < 0 or degrees > 180:
+            return Response(response="Bad request value", status=400)
 
-    numberOfTones = int(request.json["numberOfTones"])
+        servo.rotate_servo(degrees)
 
-    for _ in range(numberOfTones):
-        buzzer.sound(440)
-        time.sleep(0.25)
-        buzzer.sound_off()
-        time.sleep(0.25)
+        return Response(
+            response="Turning mast to {} degrees".format(degrees), status=200
+        )
 
-    return Response(response="Done", status=200)
+    @staticmethod
+    def move():
+        if request.json is None:
+            return Response(response="Bad request format", status=400)
 
+        action = request.json["action"]
+        arg = int(request.json["arg"])
 
-@app.route(config.LIGHT_PATH, methods=["POST"])
-def light():
-    if request.json is None:
-        return Response(response="No payload", status=400)
-    payload = request.json
+        if action == "move":
+            goPiGo.drive_cm(arg)
+            resp = "Moving {} cm".format(arg)
+            status = 200
+        elif action == "turn":
+            goPiGo.turn_degrees(arg)
+            resp = "Turning {} deg".format(arg)
+            status = 200
+        else:
+            resp = "Bad action request"
+            status = 400
 
-    if (payload.get("numberOfBlinks") and payload.get("switchLed")) or (
-        not payload.get("numberOfBlinks") and not payload.get("switchLed")
-    ):
-        return Response(response="Bad request format", status=400)
-
-    if payload.get("numberOfBlinks"):
-        numberOfBlinks = int(payload["numberOfBlinks"])
-        for _ in range(numberOfBlinks):
-            led.light_max()
-            time.sleep(0.25)
-            led.light_off()
-            time.sleep(0.25)
-        return Response(response="Blinked {} times".format(numberOfBlinks), status=200)
-    elif led.is_off():
-        led.light_max()
-        return Response(response="Turned LED on", status=200)
-    else:
-        led.light_off()
-        return Response(response="Turned LED off", status=200)
+        return Response(response=resp, status=status)
 
 
 if __name__ == "__main__":
-    app.run(host=config.SERVER_HOST, port=config.SERVER_PORT)
+    service = RobotService()
+    service.run()

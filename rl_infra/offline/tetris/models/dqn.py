4907707bff3e93a8f98ce6d7b2f8f6d77e499cda
Jon <jonathan.p.lamar@gmail.com>
2023-01-30 13:46:30 -0700

    Scaffolding offline component and implementing model service in sqlite

--- rl_infra/offline/tetris/models/dqn.py
+++ rl_infra/offline/tetris/models/dqn.py
@@ -1,30 +1,6 @@
-import random
-from collections import deque
-from typing import Deque, List, TypeVar
-
 import torch
 import torch.nn as nn
 from torch.nn.functional import relu
-
-from ..online.types.environment import Protocol, Transition
-
-T = TypeVar("T", bound=Transition, covariant=False, contravariant=False)
-
-
-class ReplayMemory(Protocol[T]):
-    memory: Deque[T]
-
-    def __init__(self, capacity: int) -> None:
-        self.memory = deque([], maxlen=capacity)
-
-    def push(self, transition: T) -> None:
-        self.memory.append(transition)
-
-    def sample(self, batchSize: int) -> List[T]:
-        return random.sample(self.memory, batchSize)
-
-    def __len__(self) -> int:
-        return len(self.memory)
 
 
 class DeepQNetwork(nn.Module):
